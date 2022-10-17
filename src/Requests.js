5bc2c3a2f35b9702672599cfe16cdb5a4942644c
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2022-10-17 12:22:54 -0300

    initial commit

---
+++ src/Requests.js
@@ -0,0 +1,4 @@
+// LF_API_KEY	= "b245ec7acd2168f95ba68b4a5ad865da"
+// LF_SHARED_SECRET	= "7ec72399b959e07ae035d9d5a597b27f"
+// t http://ws.audioscrobbler.com/2.0/
+// http://www.last.fm/api/auth/?api_key=xxx
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav__left">
+        <img src="" alt="" />
+        {/* icone seta left, middle, right, heart */}
+      </div>
+      <div className="nav__middle">{/* {logo do last} */}</div>
+      <div className="nav__right">{/* {icone, 6 links, avatar} */}</div>
+    </div>
+  )
+}
+
+export default Nav
